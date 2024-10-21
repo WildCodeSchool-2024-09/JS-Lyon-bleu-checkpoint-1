e97a8ac2528932fb29f975e3b94448f812b31b31
DameDanny <161458137+DameDanny@users.noreply.github.com>
2024-10-21 10:38:57 +0200

    JS tasklist 2 step 4

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,8 +5,17 @@ imgAvatar.addEventListener("click", () => {
 
 const nameDisplayed = document.getElementById("firstname");
 const nameButton = document.getElementById("nameButton");
+const sectionBackground = document.getElementsByClassName("pink-bg");
+const pinkText = document.getElementsByClassName("pink-text");
 nameButton.addEventListener("click", () => {
+	const colorToDisplay = prompt("Entrez la couleur voulue");
 	const nameToDisplay = prompt("Ecrivez votre nom");
 	nameDisplayed.style.color = "white";
 	nameDisplayed.innerText = nameToDisplay;
+	for (let i = 0; i < sectionBackground.length; i++) {
+		sectionBackground[i].style.backgroundColor = `${colorToDisplay}`;
+	}
+	for (let i = 0; i < pinkText.length; i++) {
+		pinkText[i].style.color = `${colorToDisplay}`;
+	}
 });
