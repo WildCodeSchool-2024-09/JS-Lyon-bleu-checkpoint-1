4fe1ae4b89c5c29dbe9190f0d43d60ff7e0a675a
User-Alnz <alexandre.anavoizard@gmail.com>
2024-10-21 12:35:33 +0200

    Home save step 4 done Js

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,9 @@
 const avatar = document.getElementById("avatar");
 const buttonChangeColor = document.getElementById("changeColorText");
 const changeName = document.getElementById("firstname");
+const getMainBlockDescription =  document.getElementsByClassName("description pink-bg");
+const  changefontColor = document.getElementsByClassName("pink-text");
+const changeFooterColor = document.getElementsByClassName("description pink-bg");
 
 
 avatar.addEventListener("click", ()=>{
@@ -10,9 +13,13 @@ avatar.addEventListener("click", ()=>{
 
 buttonChangeColor.addEventListener("click", () =>
 {
-
-    const   retriverPromptAnswer = prompt(); 
+    const   color = prompt("Enter color"); 
+    const   retriverPromptAnswer = prompt("Enter your name");
 
     changeName.innerHTML = retriverPromptAnswer;
     changeName.style.color = "inherit";
+    getMainBlockDescription[0].style.background = color;
+
+    [...changefontColor].forEach((element) => element.style.color = color);  
+    changeFooterColor[1].style.background = color;
 })
