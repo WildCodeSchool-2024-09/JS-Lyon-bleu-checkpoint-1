ea8c5eb8b7cb5fd03816bc2f27c0b5a2a8e113fb
BlueSetbyeol <setbyeol.c@gmail.com>
2024-10-21 12:00:44 +0200

    finished part 2 JS without bonus

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,17 +4,32 @@ avatar.addEventListener("click", function () {
   avatar.src = "./image/avatar.svg";
 });
 
-const firstname = document.getElementById("firstname");
+const firstName = document.getElementById("firstname");
 const description = document.querySelector(".description");
 const buttonName = document.createElement("button");
 description.appendChild(buttonName);
 buttonName.innerText = "Modifiy Text and color";
-buttonName.style.color = "#f5f5f5";
-buttonName.style.border = "#f5f5f5 1px solid";
-buttonName.style.borderRadius = "5px";
+buttonName.style.color = "var(--light)";
+buttonName.style.border = "var(--borderTemplate) var(--light)";
+buttonName.style.borderRadius = "var(--radius)";
 buttonName.style.backgroundColor = "#F7BED5";
 buttonName.style.padding = "10px 7px";
+const pinkBg = document.querySelectorAll(".pink-bg");
+const pinkText = document.querySelectorAll(".pink-text");
+
+let yourColor = null;
+let yourName = null;
 
 buttonName.addEventListener("click", function () {
-  buttonName = prompt("What's your name ?");
+  yourColor = prompt("What color do you like ?");
+  yourName = prompt("What's your first name ?");
+  firstName.innerText = yourName;
+  firstName.style.color = "var(--light)";
+  for (let i = 0; i < pinkBg.length; i++) {
+    pinkBg[i].style.backgroundColor = "#750ff7";
+  }
+  buttonName.style.backgroundColor = "#750ff7";
+  for (let j = 0; j < pinkText.length; j++) {
+    pinkText[j].style.color = "#750ff7";
+  }
 });
