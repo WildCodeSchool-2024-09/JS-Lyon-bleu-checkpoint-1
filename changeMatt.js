e61fadd4aa7f43b57185dfacc6ba0f831537d619
STEPHEN BACHER <bacher.stephen@gmail.com>
2024-10-21 12:16:51 +0200

    3rd commit

---
+++ changeMatt.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("button")
+   document.querySelector('#button').addEventListener('click', function() { }
+    var userAdjective = prompt("Enter your name");
+    alert (userAdjective);  
+
+
+const button = document.getElementById('button');
+ const div = document.getElementById('firstname');
+        function changeContent() {div.innerHTML = 'prompt'};
+    }
+button.addEventListener('click', changeContent);
+
---
+++ site/js/portfolio.js
@@ -0,0 +1,31 @@
+<><script>
+    const button = document.getElementById("button")
+    document.querySelector('#button').addEventListener('click', function() { }
+    var userAdjective = prompt("Enter your name");
+    alert (userAdjective);
+    }
+    }
+
+</script>  
+
+<script> 
+const button = document.getElementById('button');
+ const div = document.getElementById('firstname');
+
+        // define the function to change the HTML content
+        function changeContent() {div.innerHTML = '<p>New HTML content!</p>'};
+    }
+button.addEventListener('click', changeContent);
+
+</script>
+
+
+
+
+
+
+
+
+
+const newAvatar = document.getElementById("avatar")
+    newAvatar
