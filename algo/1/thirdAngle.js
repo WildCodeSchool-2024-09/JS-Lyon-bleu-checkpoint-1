91ba09c25b368c1def2fe0fbea56c002f7510e99
nadir haddouche <nads@MacBook-Pro-de-Nadir.local>
2024-10-21 09:14:43 +0200

    first commit for test

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,8 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+  let result = (180 - a - b) === 0;
+  console.log(result);
 }
 
 module.exports = thirdAngle;
