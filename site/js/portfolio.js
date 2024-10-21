9c12c37c63e882e45614bb03bd81ea4b2493ba64
nadir haddouche <nads@MacBook-Pro-de-Nadir.local>
2024-10-21 12:39:34 +0200

    il ne reste qu'une etape JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,3 +4,19 @@ changeAvatar.addEventListener('click', function () {
     changeAvatar.src = "./image/avatar.svg";
     changeAvatar.alt = "Beautiful new avatar";
 });
+
+
+const newName = document.getElementById('firstname');
+const button = document.querySelector('.button1');
+const changeBg = document.querySelector('.pink-bg');
+
+button.addEventListener('click', function () {
+let resultName = prompt("Enter your name");
+let resultColor = prompt("Enter a color");
+newName.innerHTML=resultName;
+newName.style.color='white';
+changeBg.style.background=resultColor;
+button.style.background=resultColor;
+
+});
+
