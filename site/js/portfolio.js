f91843dd0c7bbcb079fa9dcd7a664863f2a2d33c
elhedi <hheeddii87@gmail.com>
2024-10-21 12:27:31 +0200

    je suis sur la derniere etape je galere

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,7 +2,7 @@ const avatar = document.querySelector('#avatar');
 const firstname = document.querySelector('#firstname')
 const bouton = document.querySelector('#btnmod')
 const couleur = document.querySelector('.description')
-const pinkbg = document.querySelector('.pink-bg')
+const pinktxt = document.querySelectorAll('.pink-bg')
 
 avatar.addEventListener('click',function(){
     avatar.src = "/site/image/avatar.svg"
@@ -11,11 +11,10 @@ avatar.addEventListener('click',function(){
 bouton.addEventListener('click', function(){
     const prompt1 = prompt("quelle est votre prenom?");
     const prompt2 = prompt("choisi ta couleur?");
-    const prompt3 = prompt("choisi la couleur des elements")
     firstname.innerText = `${prompt1}`;
     firstname.style.color = 'white';
     couleur.style.backgroundColor = `#${prompt2}`;
-    pinkbg.style.color = '#750ff7';
+    pinktxt.style.color = '#750ff7';
 
 });
 
